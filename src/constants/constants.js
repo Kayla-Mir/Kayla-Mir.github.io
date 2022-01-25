35d7a60b8cc003314fe0bacf0527ee1e6da9697a
Kayla Mir <kayla.mir32@gmail.com>
2022-01-24 18:37:10 -0600

    working portfolio

---
+++ src/constants/constants.js
@@ -0,0 +1,52 @@
+export const projects = [
+  {
+    title: 'Gathering the Magic',
+    description: 
+      `Is a full stack, full CRUD, React based collection manager and deck builder for Magic the Gathering.
+      I built this application for people like my husband and I who have far too many magic cards to keep track 
+      of. Or for people that just want a convenient way to digitally handle their physical collection.`,
+    image: '/images/gathering-the-magic.png',
+    tags: ['React', 'Redux', 'Chart.js', 'MaterialUI', 'PostgreSQL'],
+    source: 'https://vimeo.com/667413661',
+    visit: 'https://github.com/Kayla-Mir/gathering-the-magic',
+    id: 0,
+  },
+  {
+    title: 'React Gallery',
+    description:
+      `Is an application that allows you display your photos, add descriptions, and keep track of likes. 
+      Clicking on a card will flip the image over and display a brief description of the image.`,
+    image: '/images/react-gallery.png',
+    tags: ['React', 'MaterialUI', 'SweetAlert', 'Axios'],
+    source: 'https://google.com',
+    visit: 'https://github.com/Kayla-Mir/react-gallery',
+    id: 1,
+  },
+  {
+    title: 'To Do List App',
+    description: 
+      `This is a full CRUD To Do List application, users can add new tasks to the table, delete unwanted 
+      tasks, and completing the task by clicking on the button will record the time that task was completed.`,
+    image: '/images/to-do-list.png',
+    tags: ['JQuery', 'Javascript', 'Express', 'Node.js'],
+    source: 'https://google.com',
+    visit: 'https://github.com/Kayla-Mir/sql-to-do-list',
+    id: 2,
+  },
+  {
+    title: 'Movie Sagas',
+    description: "This is a code repository for the corresponding video tutorial. In this video, we will create a full Realtime Chat Application",
+    image: '/images/movie-sagas.png',
+    tags: ['React', 'Redux', 'Node.js', 'MaterialUI'],
+    source: 'https://google.com',
+    visit: 'https://github.com/Kayla-Mir/movie-sagas',
+    id: 3,
+  },
+];
+
+export const TimeLineData = [
+  { year: '1/12/21', text: 'Started working as a CNA', },
+  { year: 2020, text: 'Moved to Minneapolis', },
+  { year: 2021, text: 'Became a Full Stack Student at Prime Digital Academy', },
+  { year: 2022, text: 'Graduate Feb 18th as a Full Stack Developer'}
+];
