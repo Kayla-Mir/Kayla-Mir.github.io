c7867f4ce92bea3367bfa3ceb9b521add8c10096
Kayla Mir <kayla.mir32@gmail.com>
2022-02-24 17:55:39 -0600

    updated projects to include group project

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -3,13 +3,14 @@ import React from 'react';
 import { BlogCard, CardInfo, ExternalLinks, GridContainer, HeaderThree, Hr, Tag, TagList, TitleContent, UtilityList, Img } from './ProjectsStyles';
 import { Section, SectionDivider, SectionTitle } from '../../styles/GlobalComponents';
 import { projects } from '../../constants/constants';
+import Button from '../../styles/GlobalComponents/Button';
 
 const Projects = () => (
   <Section  id="projects">
     <SectionDivider />
     <SectionTitle main>Projects</SectionTitle>
     <GridContainer>
-      {projects.map(({ id, image, title, description, tags, source, visit}) => (
+      {projects.map(({ id, image, title, description, tags, source, visit, deployed}) => (
         <BlogCard key={id}>
           <Img src={image} />
           <TitleContent>
@@ -27,11 +28,20 @@ const Projects = () => (
           </div>
           <UtilityList>
             <ExternalLinks href={visit}>Code</ExternalLinks>
-            {id === 0 && <ExternalLinks href={source}>Demo</ExternalLinks>}
+            {deployed !== '' && <ExternalLinks href={deployed}>Live Version</ExternalLinks>}
+            {source !== '' && <ExternalLinks href={source}>Demo</ExternalLinks>}
           </UtilityList>
         </BlogCard>
       ))}
     </GridContainer>
+    <div style={{margin: 'auto'}}>
+      <Button 
+        style={{padding: '20px'}}
+        onClick={() => window.location = 'https://www.github.com/Kayla-Mir'}
+      >
+        More of my work!
+      </Button>
+    </div>
   </Section>
 );
 
--- src/constants/constants.js
+++ src/constants/constants.js
@@ -2,25 +2,40 @@ export const projects = [
   {
     title: 'Gathering the Magic',
     description: 
-      `Is a full stack, full CRUD, React based collection manager and deck builder for Magic the Gathering.
+      `A full stack, full CRUD, React based collection manager and deck builder for Magic the Gathering.
       I built this application for people like my husband and I who have far too many magic cards to keep track 
       of. Or for people that just want a convenient way to digitally handle their physical collection.`,
     image: '/images/gathering-the-magic.png',
     tags: ['React', 'Redux', 'Chart.js', 'MaterialUI', 'PostgreSQL'],
     source: 'https://vimeo.com/667413661',
     visit: 'https://github.com/Kayla-Mir/gathering-the-magic',
+    deployed: 'https://gathering-the-magic.herokuapp.com/',
     id: 0,
+  },
+  {
+    title: 'SPUR Connect',
+    description: 
+      `Is a full stack, full CRUD, React based collection manager and deck builder for Magic the Gathering.
+      I built this application for people like my husband and I who have far too many magic cards to keep track 
+      of. Or for people that just want a convenient way to digitally handle their physical collection.`,
+    image: '/images/spur-connect.png',
+    tags: ['React', 'Redux', 'MaterialUI', 'PostgreSQL'],
+    source: '',
+    visit: 'https://github.com/Kayla-Mir/SPURConnect',
+    deployed: 'https://spur-connect.herokuapp.com/',
+    id: 1,
   },
   {
     title: 'React Gallery',
     description:
-      `Is an application that allows you display your photos, add descriptions, and keep track of likes. 
+      `An application that allows you display your photos, add descriptions, and keep track of likes. 
       Clicking on a card will flip the image over and display a brief description of the image.`,
     image: '/images/react-gallery.png',
     tags: ['React', 'MaterialUI', 'SweetAlert', 'Axios'],
-    source: 'https://google.com',
+    source: '',
     visit: 'https://github.com/Kayla-Mir/react-gallery',
-    id: 1,
+    deployed: '',
+    id: 2,
   },
   {
     title: 'To Do List App',
@@ -29,19 +44,22 @@ export const projects = [
       tasks, and completing the task by clicking on the button will record the time that task was completed.`,
     image: '/images/to-do-list.png',
     tags: ['JQuery', 'Javascript', 'Express', 'Node.js'],
-    source: 'https://google.com',
+    source: '',
     visit: 'https://github.com/Kayla-Mir/sql-to-do-list',
-    id: 2,
-  },
-  {
-    title: 'Movie Sagas',
-    description: "This is a code repository for the corresponding video tutorial. In this video, we will create a full Realtime Chat Application",
-    image: '/images/movie-sagas.png',
-    tags: ['React', 'Redux', 'Node.js', 'MaterialUI'],
-    source: 'https://google.com',
-    visit: 'https://github.com/Kayla-Mir/movie-sagas',
+    deployed: '',
     id: 3,
   },
+  // {
+  //   title: 'Movie Sagas',
+  //   description: 
+  //     ``,
+  //   image: '/images/movie-sagas.png',
+  //   tags: ['React', 'Redux', 'Node.js', 'MaterialUI'],
+  //   source: '',
+  //   visit: 'https://github.com/Kayla-Mir/movie-sagas',
+  //   deployed: '',
+  //   id: 4,
+  // },
 ];
 
 export const TimeLineData = [
